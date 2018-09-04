cbbeddc5dc2abad63066a4bdf1125f0b6cd8b79c
djknit <djknit@gmail.com>
2018-09-03 21:58:43 -0500

    some functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // creating character objects
-// constructor
 function Character(name, hp, attack, counter) {
     this.name = name;
     this.startingHealthPoints = hp;
@@ -8,8 +7,31 @@ function Character(name, hp, attack, counter) {
     this.healthPoints = hp;
     this.attackPoints = attack;
     this.counterAttackPoints = counter;
-    var iconId = "#" + name.toLowerCase();
-    this.icon = $(iconId);
+    this.icon = $("#" + name.toLowerCase());
+    this.isDefeated = false;
+    this.incrementAttack = function() {
+        this.attackPoints = this.attackPoints + this.startingAttackPoints;
+    };
+    this.displayHp = function() {
+        $(".hp." + this.name.toLowerCase()).html(this.healthPoints);
+    };
+    this.reduceHp = function(points) {
+        this.healthPoints = this.healthPoints - points;
+        this.displayHp();
+    };
+    this.attack = function(defender0) {
+        defender0.reduceHp(this.attackPoints);
+    };
+    this.defend = function(attacker0) {
+        attacker0.reduceHp(this.counterAttackPoints);
+    };
+    this.reset = function() {
+        this.healthPoints = this.startingHealthPoints;
+        this.displayHp();
+        this.attackPoints = this.startingAttackPoints;
+        this.counterAttackPoints = this.startingCounterAttackPoints;
+        this.isDefeated = false;
+    };
 }
 
 var characters = [];
@@ -17,3 +39,65 @@ characters[0] = new Character("Cupcake", 60, 30, 40);
 characters[1] = new Character("Cheeseburger", 90, 25, 60);
 characters[2] = new Character("Artichoke", 180, 15, 25);
 characters[3] = new Character("Carrot", 200, 30, 40);
+
+var attacker = {};
+var defender = {};
+gamePhase = "characterSelection"
+
+function newGame() {
+    gamePhase = "characterSelection"
+    for (i = 0; i < characters.length; i++) {
+        characters[i].reset();
+        $("#characters").append(characters[i].icon);
+        $("#characters").append(" ");
+    }
+}
+
+function chooseCharacter(character0) {
+    if (gamePhase === "characterSelection") {
+        attacker = character0;
+        for (i = 0; i < characters.length; i++) {
+            if (characters[i] === attacker) {
+                $("#your_character").append(characters[i].icon);
+            }
+            else {
+                $("#enemies").append(characters[i].icon);
+                $("#enemies").append(" ");
+            }
+        }
+        gamePhase = "enemySelection";
+    }
+}
+
+function chooseDefender(enemy0) {
+    if (gamePhase === "enemySelection") {
+        gamePhase = "battle";
+        defender = enemy0;
+        $("#defender").append(enemy0.icon);
+    }
+}
+
+characters[0].icon.on("click", function() {
+    chooseDefender(characters[0]);
+    chooseCharacter(characters[0]);
+});
+characters[1].icon.on("click", function() {
+    chooseDefender(characters[1]);
+    chooseCharacter(characters[1]);
+});
+characters[2].icon.on("click", function() {
+    chooseDefender(characters[2]);
+    chooseCharacter(characters[2]);
+});
+characters[3].icon.on("click", function() {
+    chooseDefender(characters[3]);
+    chooseCharacter(characters[3]);
+});
+
+$("#attack_btn").on("click", function() {
+    if (gamePhase === "battle") {
+        attacker.attack(defender);
+        defender.defend(attacker);
+        attacker.incrementAttack();
+    }
+});
