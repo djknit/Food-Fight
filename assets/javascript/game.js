97d1ada0d925eec6091af45ae5d04586014f1d90
djknit <djknit@gmail.com>
2018-09-04 17:58:37 -0500

    fixed bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,16 +47,21 @@ var enemiesLeft = 3;
 
 function newGame() {
     gamePhase = "characterSelection";
-    $("#info").html("Choose your character by clicking on it.");
-    for (i = 0; i < characters.length; i++) {
-        characters[i].reset();
+    for (let i = 0; i < characters.length; i++) {
         $("#characters").append(characters[i].icon);
         $("#characters").append(" ");
+        if (characters[i].isDefeated) {
+            characters[i].icon.on("click", function() {
+                console.log("clicked");
+                chooseDefender(characters[i]);
+                chooseCharacter(characters[i]);
+            });
+        }
+        characters[i].isDefeated = false;
+        characters[i].reset();
     }
     enemiesLeft = 3;
 }
-
-newGame();
 
 function chooseCharacter(character0) {
     if (gamePhase === "characterSelection") {
@@ -77,6 +82,7 @@ function chooseCharacter(character0) {
 
 function chooseDefender(enemy0) {
     if (gamePhase === "enemySelection") {
+        console.log("enemy selection: " + enemy0.name);
         defender = enemy0;
         $("#defender").append(enemy0.icon);
         gamePhase = "battle";
@@ -84,43 +90,65 @@ function chooseDefender(enemy0) {
     }
 }
 
-characters[0].icon.on("click", function() {
-    chooseDefender(characters[0]);
-    chooseCharacter(characters[0]);
-});
-characters[1].icon.on("click", function() {
-    chooseDefender(characters[1]);
-    chooseCharacter(characters[1]);
-});
-characters[2].icon.on("click", function() {
-    chooseDefender(characters[2]);
-    chooseCharacter(characters[2]);
-});
-characters[3].icon.on("click", function() {
-    chooseDefender(characters[3]);
-    chooseCharacter(characters[3]);
-});
+newGame();
+$("#info").html("Choose a character to fight with.");
+
+for (let i = 0; i < characters.length; i++) {
+    characters[i].icon.on("click", function() {
+        console.log(characters[i].name + " clicked");
+        chooseDefender(characters[i]);
+        chooseCharacter(characters[i]);
+    });
+}
+
+// characters[0].icon.on("click", function() {
+//     console.log("cupcake clicked");
+//     chooseDefender(characters[0]);
+//     chooseCharacter(characters[0]);
+// });
+// characters[1].icon.on("click", function() {
+//     console.log("burger clicked");
+//     chooseDefender(characters[1]);
+//     chooseCharacter(characters[1]);
+// });
+// characters[2].icon.on("click", function() {
+//     console.log("artichoke clicked");
+//     chooseDefender(characters[2]);
+//     chooseCharacter(characters[2]);
+// });
+// characters[3].icon.on("click", function() {
+//     console.log("carrot clicked");
+//     chooseDefender(characters[3]);
+//     chooseCharacter(characters[3]);
+// });
 
 $("#attack_btn").on("click", function() {
     if (gamePhase === "battle") {
         attacker.attack(defender);
         if (defender.healthPoints <= 0) {
+            defender.isDefeated = true;
+            console.log("Enemy defeated");
             defender.icon.remove();
             enemiesLeft--;
+            console.log(enemiesLeft + " enemies left");
             if (enemiesLeft === 0) {
                 $("#info").html("You won! Choose another character to fight with!");
                 newGame();
+                console.log("win");
             }
             else {
                 gamePhase = "enemySelection";
                 $("#info").html("Choose an enemy to battle next.");
             }
         }
+        else {
         defender.defend(attacker);
         attacker.incrementAttack();
         if (attacker.healthPoints <= 0) {
             $("#info").html("You lost. Try again. Choose a character.");
             newGame();
+            console.log("loss");
         }
+    }
     }
 });
